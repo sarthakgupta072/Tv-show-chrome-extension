a0fc060eb190217e89895473b2da27e68f2ce115
Sarthak Gupta <sarthakgupta072@gmail.com>
2023-06-07 23:09:23 +0530

    feat: add context menus

--- background.js
+++ background.js
@@ -1,5 +1,30 @@
 chrome.runtime.onInstalled.addListener((details) => {
-    console.log(details)
+    chrome.contextMenus.create({
+        "title": "Test Context Menu",
+        "id": "contextMenu1",
+        "contexts": ["page", "selection"]
+    })
+
+    chrome.contextMenus.onClicked.addListener((event) => {
+        console.log(event)
+    })
+
+    chrome.contextMenus.create({
+        "title": "Test Context Menu 2",
+        "id": "contextMenu2",
+        "contexts": ["page", "selection"],
+        "parentId": "contextMenu1"
+    })
+
+    chrome.contextMenus.create({
+        "title": "Test Context Menu 3",
+        "id": "contextMenu3",
+        "contexts": ["page", "selection"],
+        "parentId": "contextMenu1"
+    })
+
+    
+
 })
 
 console.log("Background script running")
