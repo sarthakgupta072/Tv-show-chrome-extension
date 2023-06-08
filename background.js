083a46b1c41ae2be56b5d35d841559064dc5d272
Sarthak Gupta <sarthakgupta072@gmail.com>
2023-06-08 21:52:50 +0530

    feat: integrate selected text imdb search

--- background.js
+++ background.js
@@ -7,24 +7,10 @@ chrome.runtime.onInstalled.addListener((details) => {
 
     chrome.contextMenus.onClicked.addListener((event) => {
         console.log(event)
+        chrome.tabs.create({
+            url: `https://www.imdb.com/find/?q=${event.selectionText}`
+        })
     })
-
-    chrome.contextMenus.create({
-        "title": "Test Context Menu 2",
-        "id": "contextMenu2",
-        "contexts": ["page", "selection"],
-        "parentId": "contextMenu1"
-    })
-
-    chrome.contextMenus.create({
-        "title": "Test Context Menu 3",
-        "id": "contextMenu3",
-        "contexts": ["page", "selection"],
-        "parentId": "contextMenu1"
-    })
-
-    
-
 })
 
 console.log("Background script running")
